faf24849710505e6e381f8d42c3bd8a7256ce992
co <cody0041@umn.edu>
2021-01-11 17:56:15 -0600

    pmenu fixes

--- pmenu.py
+++ pmenu.py
@@ -1,33 +1,39 @@
-import bpy
+"""[ Main Properties Panel ]
+"""
 
-class VirtualGoniometerControlPanel(bpy.types.Panel):
+from bpy import context as C
+from bpy.types import Panel
+
+class VirtualGoniometerControlPanel(Panel):
     """Creates a Panel in the scene context of the properties editor"""
     bl_label = "Virtual Goniometer"
     bl_idname = "SCENE_PT_layout"
     bl_space_type = 'PROPERTIES'
     bl_region_type = 'WINDOW'
     bl_context = "scene"
+    ANGLE_PRECISION = 1
     def draw(self, context):
         layout = self.layout
         scene = context.scene
         
-        if bpy.context.active_object != None:
-            self.ANGLE_PRECISION = 1
-            cpi = bpy.context.active_object.cs_individual_VG_
-
+        if C.active_object is not None:
+            cpi = C.active_object.cs_individual_VG_
+            cpo = C.active_object.material_pairs
             # Create an alligned column
             layout.label(text=" Projection Controls:")
             col = layout.row(align=True)
-            col.prop(scene.cs_overall_VG_,'number_of_nearest_neighbors', text = 'Points to include in auto-generated region')
+            nonn_text = 'Points to include in auto-generated region'
+            col.prop(scene.cs_overall_VG_, 'number_of_nearest_neighbors', text=nonn_text)
             col.prop(scene.cs_overall_VG_, 'number_of_random_projections')
             row = layout.row()
             
             
-            if bpy.context.active_object.cs_individual_VG_.base_material is None:
-                base_material.attempt_recovery()
+            if C.active_object.cs_individual_VG_.base_material is None:
+                #base_material.attempt_recovery()
+                pass
             
-            row.prop(bpy.context.active_object.cs_individual_VG_.base_material,
-                "diffuse_color",text="Base_Color")
+            row.prop(C.active_object.cs_individual_VG_.base_material,
+                "diffuse_color", text="Base_Color")
                 
             # Big render button
             layout.label(text="Perform Angle Measurement:")
@@ -46,42 +52,47 @@ class VirtualGoniometerControlPanel(bpy.types.Panel):
             col = layout.column(align=True)
             #col.operator("object.show_selection_circle")
             #col.operator("object.simplify_mesh")
-            col.operator("object.center_sample")#, icon_value=co_arrays.custom_icons["center"].icon_id)
-            #col.operator("object.undo_last_point", icon_value=co_arrays.custom_icons["undo"].icon_id)
-            col.operator("object.clear_selection")#, icon_value=co_arrays.custom_icons["reset"].icon_id)
+            col.operator("object.center_sample")
+            #, icon_value=co_arrays.custom_icons["center"].icon_id)
+            #col.operator("object.undo_last_point", icon_value=custom_icons["undo"].icon_id)
+            col.operator("object.clear_selection")
+            #, icon_value=co_arrays.custom_icons["reset"].icon_id)
             
             data_box = layout.box()
             data_box.label(text='Angle Data:')
             
-            if not(bpy.context.active_object is None):
+            if C.active_object is not None:
                 
-                if len(active_pairs)==0:
-                    data_box.label(text="Selected Angles will show here.",icon="ADD")
+                if len(cpo) == 0:
+                    data_box.label(text="Selected Angles will show here.", icon="ADD")
+                    
                 else:
-                    for i, pair in enumerate(active_pairs):
+                    
+                    for i, pair in enumerate(cpo):
                         #print(patch)
                         side_colors = data_box.row(align=True)
                         side_colors.scale_x = 0.22
                         
-                        side_colors.prop(pair.material_1.material, "diffuse_color",text="")
-                        side_colors.prop(pair.material_2.material, "diffuse_color",text="")
+                        side_colors.prop(pair.material_1.material, "diffuse_color", text="")
+                        side_colors.prop(pair.material_2.material, "diffuse_color", text="")
                         
                         sub = side_colors.row(align=True)
                         
                         
 
-                        sub.prop(pair.bsp,'name',text = "")
-                        sub.label(text= '( '+str(round(pair.theta, self.ANGLE_PRECISION))+'° )')
+                        sub.prop(pair.bsp, 'name', text="")
+                        sub.label(text='( '+str(round(pair.theta, self.ANGLE_PRECISION))+'° )')
                         
                         edits = sub.row(align=True)
                         edits.scale_x = 0.45
                         
-                        edits.operator("view3d.editside",text = '1',depress=cpi.depressed[0]).options = (1,i)
-                        edits.operator("view3d.editside",text = '2',depress=cpi.depressed[1]).options = (2,i)
+                        d = cpi.depressed
+                        edits.operator("view3d.editside", text='1', depress=d[0]).options = (1, i)
+                        edits.operator("view3d.editside", text='2', depress=d[1]).options = (2, i)
                         
                         delp = sub.row(align=True)
                         delp.scale_x = 0.4
-                        delp.operator("object.deletepatch",text=" ",icon="CANCEL").patch_int = i
+                        delp.operator("object.deletepatch", text=" ", icon="CANCEL").patch_int = i
                         #delp.label(text=" ")
                      
             else:
